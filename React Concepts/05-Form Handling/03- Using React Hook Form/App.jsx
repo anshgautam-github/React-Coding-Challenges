ae8d71c465c5b4eba4ca130053d33439aaaa0d78
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-05-27 13:27:40 +0530

    Update App.jsx

--- React Concepts/05-Form Handling/03- Using React Hook Form/App.jsx
+++ React Concepts/05-Form Handling/03- Using React Hook Form/App.jsx
@@ -11,7 +11,14 @@ const App = () => {
     <div className="p-4">
       <h5>Form</h5>
       <form action="" onSubmit={handleSubmit((data)=>console.log(data))}>
-        <input {...register("name")} className="border-2 px-1 py-1 mx-2" type="text" placeholder="Name"autoComplete="off"/>
+        <input 
+         {...register("name",{
+          required:true,
+          minLength:{value:3, message:"Min length at least 3"},
+          maxLength:{value:6, message:"Max length at most 6"},
+         })} 
+         className="border-2 px-1 py-1 mx-2" type="text" placeholder="Name"autoComplete="off"/>
+       { errors.name && <p>{errors.name.message}</p>}
         <input {...register("password")} type="password" placeholder="password"autoComplete="off"/>
         <input
          className="px-3 py-1 bg-sky-600 rounded-sm text-white text-sm"
