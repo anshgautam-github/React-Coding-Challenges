d7f9b83a81c918f420ddb152c592a9fa1f519049
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-07-27 15:27:15 +0530

    Update App.jsx

--- React Concepts/04-Props/Basic Props/App.jsx
+++ React Concepts/04-Props/Basic Props/App.jsx
@@ -1,3 +1,16 @@
+/*
+📁 component/                  // Root folder of your project
+├── 📁 node_modules/           // Dependencies installed by npm
+├── 📁 public/                // Static files (e.g., index.html)
+├── 📁 src/                   // Source code
+│   ├── 📁 assets/            // Image, icons, etc.
+│   │   └── react.svg         // Example image file
+│   └── 📁 components/        // Reusable UI components
+│       ├── UserCard.css      // Styling for UserCard
+│       └── UserCard.jsx      // React component
+*/
+
+
 import UserCard from './components/UserCard'
 import logo from './assets/react.svg'
 function App() {
